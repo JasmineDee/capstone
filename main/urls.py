230c4768248596393b458b72b8849082817d958e
mac <mac@macs-Air.hsd1.wa.comcast.net>
2022-07-27 11:23:06 -0700

    test

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,7 @@ from .views import TaskList, TaskDetail, TaskCreate
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
-    path('task-create', TaskCreate.as_view(), name='task-create')
+    path('task-create/', TaskCreate.as_view(), name='task-create'),
 
 
 
